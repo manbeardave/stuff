a274c5dd0fcfda62485c0d8b23b49243dcb76174
manbeardave <dave.hines@vervemobile.com>
2014-12-08 16:08:23 -0800

    Stuff for processing

--- web/processinfo.php
+++ web/processinfo.php
@@ -1,5 +1,5 @@
 <html>
-<link rel="stylesheet" href="web/forms.css" type="text/css" media="screen">
+<link rel="stylesheet" href="forms.css" type="text/css" media="screen">
 <?php
 
   $date=$_POST['date'];
@@ -31,15 +31,15 @@
 				// echo $data["company"];
 				// echo $data["date"];
 
-$pdf_file_url = '/Verve_Example_Contract.pdf';
+$pdf_file_url = 'https://s3-us-west-1.amazonaws.com/pdfgeneration/Verve_Example_Contract.pdf';
 include 'createXFDF.php';
 $xfdf = createXFDF( $pdf_file_url, $data );
 
 
 
-$result_directory = dirname(__FILE__) . '/xfdf_files';
+$result_directory = 'https://s3-us-west-1.amazonaws.com/pdfgeneration/xfdf_files';
 $xfdf_file = $data['corp_name'] . '-' . $data['NAME'] . '.xfdf';
-$xfdf_file_path = $result_directory . '/' . $xfdf_file;
+$xfdf_file_path = 'https://s3-us-west-1.amazonaws.com/pdfgeneration/xfdf_files' . '/' . $xfdf_file;
 
 if( $fp = fopen( $xfdf_file_path, 'w' ) )
 {
@@ -47,10 +47,10 @@ if( $fp = fopen( $xfdf_file_path, 'w' ) )
 };
 fclose($fp);
 
-$pdf_template_path = dirname(__FILE__) . '/Verve_Example_Contract.pdf';
+$pdf_template_path = 'https://s3-us-west-1.amazonaws.com/pdfgeneration/Verve_Example_Contract.pdf';
 $pdftk = '/usr/bin/pdftk';
-$pdf_name = dirname(__FILE__) . '/' . 'pdf_files' . '/' . $data['corp_name'] . '-' . $data['NAME'] . '.pdf';
-$command = "sudo pdftk $pdf_template_path fill_form '$xfdf_file_path' output '$pdf_name' flatten";
+$pdf_name = 'https://s3-us-west-1.amazonaws.com/pdfgeneration/pdf_files' . $data['corp_name'] . '-' . $data['NAME'] . '.pdf';
+$command = "$pdftk $pdf_template_path fill_form '$xfdf_file_path' output '$pdf_name' flatten";
 
 $pdf_url = 'https://glacial-journey-4511.herokuapp.com/pdf_files/' . $data['corp_name'] . '-' . $data ['NAME'] . '.pdf';
 
@@ -66,7 +66,7 @@ echo "<div class='bg_i'>
 
 
 </div></div>
-<object data='$pdf_url' type='application/pdf' class='right_pdf' height='1000px'><p>It appears you don't have Adobe Reader on your machine - click<a href='$pdf_url'>here</a>to preview your PDF!</p></object>
+<object data='$pdf_name' type='application/pdf' class='right_pdf' height='1000px'><p>It appears you don't have Adobe Reader on your machine - click<a href='$pdf_url'>here</a>to preview your PDF!</p></object>
 </div>"
 
 
